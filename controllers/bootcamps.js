742368e0d2837cfd48b01fa2728b8e67bf48f8a4
KubikArmin <kubikarminmartin@gmail.com>
2023-11-28 09:05:14 +0100

    :goal_net: Hibakezelő osztály alkalmazása

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,56 +1,15 @@
+const ErrorResponse = require('../utils/errorResponse')
 const Training = require("../models/Training");
-// @desc   Get all trainings
-// @route  GET /api/trainings
-// @access Public
-// exports.getTrainings = (req, res, next) => {
-//   res
-//     .status(200)
-//     .json({ success: true, msg: "Show all trainings", hello: req.hello });
-// }; // @desc   Get single training
-// // @route  GET /api/trainings/:id
-// // @access Public
-// exports.getTraining = (req, res, next) => {
-//   res.status(200).json({ success: true, msg: `Get training ${req.params.id}` });
-// }; // @desc   Create new training
-// @route  POST /api/trainings
-// @access Private
-// exports.createTraining = (req, res, next) => {
-//   console.log(req.body);
-//   res.status(200).json({ success: true, msg: "Create new training" });
-// }; // @desc   Update training
-// @route  PUT /api/trainings/:id
-// @access Private
-// exports.updateTraining = (req, res, next) => {
-  
-//   res
-//     .status(200)
-//     .json({ success: true, msg: `Update training ${req.params.id}` });
-// }; // @desc   Delete training
-// @route  DELETE /api/trainings/:id
-// @access Private
-// exports.deleteTraining = (req, res, next) => {
-//   res
-//     .status(200)
-//     .json({ success: true, msg: `Delete training ${req.params.id}` });
-// };
-// @desc   Get all trainings
-// @route  GET /api/trainings
-// @access Public
-// exports.getTrainings = (req, res, next) => {
-//   res
-//     .status(200)
-//     .json({ success: true, msg: "Show all trainings", hello: req.hello });
-// };
 
 exports.createTraining = async (req, res, next) => {
-  try {
-    const training = await Training.create(req.body);
-    res.status(201).json({ success: true, data: training });
-  } catch (error) {
-    res.status(400).json({ success: false });
-  }
-};
-
+            try {
+              const training = await Training.create(req.body);
+              res.status(201).json({ success: true, data: training });
+            } catch (error) {
+              res.status(400).json({ success: false });
+            }
+          };
+          
 exports.getTrainings = async (req, res, next) => {
   try {
     const trainings = await Training.find();
@@ -70,7 +29,7 @@ exports.getTraining = async (req, res, next) => {
     }
     res.status(200).json({ success: true, data: training });
   } catch (error) {
-    next(error)
+    next(new ErrorResponse(`A megadott id (${req.params.id}) nem létezik az adatbázisban`, 404))
   }
 };
 
