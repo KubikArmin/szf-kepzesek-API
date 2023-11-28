4ee21272b73feb44370bf37bf5d2f3cd2ae86d99
KubikArmin <kubikarminmartin@gmail.com>
2023-11-28 09:15:18 +0100

    Error szerkesztése

--- middleware/error.js
+++ middleware/error.js
@@ -3,7 +3,7 @@ const errorHandler = (err, req, res, next) => {
   console.log(err.stack);
 
   if (err.code === 11000) {
-    const message = "Duplicate field value";
+    const message = "A megadott mezőknek egyedinek kell lenniük!";
     err = new ErrorResponse(message, 400);
   }
 
